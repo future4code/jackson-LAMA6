06e67cbf51a07f6762a4e5d6f803970d04b77cad
Claudia Trevisan <ctrevisan.oliveira@gmail.com>
2020-11-27 15:41:24 -0300

    ajustes nas camadas user

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
 import { UserInput, LoginInput } from "../model/User";
-import  UserBusiness  from "../business/UserBusiness";
+import  userBusiness, { UserBusiness }  from "../business/UserBusiness";
 import { BaseDatabase } from "../data/BaseDatabase";
+import { validation } from "../utils/validation";
+import  { stringToUserRole }  from '../model/User'
 
 export class UserController {
-  async signup(req: Request, res: Response) {
+  constructor(private userBusiness: UserBusiness) {}
+  
+  public signup = async (req: Request, res: Response) =>{
     try {
       const input: UserInput = {
         email: req.body.email,
@@ -13,15 +17,14 @@ export class UserController {
         role: req.body.role
       };
 
-      // const userBusiness = new UserBusiness();
-      const token = await UserBusiness.createUser(input);
+      const token = await this.userBusiness.signup(input, validation, stringToUserRole);
 
       res.status(200).send({ token });
     } catch (error) {
       res.status(400).send({ error: error.message });
     }
 
-    // await BaseDatabase.destroyConnection();
+    await BaseDatabase.destroyConnection();
   }
 
   async login(req: Request, res: Response) {
@@ -31,8 +34,7 @@ export class UserController {
         password: req.body.password
       };
 
-      // const userBusiness = new UserBusiness();
-      const token = await UserBusiness.getUserByEmail(loginData);
+      const token = await userBusiness.login(loginData, validation);
 
       res.status(200).send({ token });
     } catch (error) {
